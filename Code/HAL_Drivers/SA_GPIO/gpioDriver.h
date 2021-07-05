7204e1f49bc72ee5ff4ff4ba9e523b341301c721
Robert Klajko <robert_klajko@epam.com>
2021-07-05 18:54:03 +0200

    GPIO HAL driver tests added

--- Code/HAL_Drivers/SA_GPIO/gpioDriver.h
+++ Code/HAL_Drivers/SA_GPIO/gpioDriver.h
@@ -52,17 +52,6 @@ typedef struct
 	uint32_t GPIO_PullUpDown_Selector;
 } GPIO_DriverGPIOConfig_s;
 
-static GPIO_DriverGPIOConfig_s GPIO_DriverGPIOPeripherals[GPIO_DriverGPIOPeripheral_End] =
-	{
-		[GPIO_DriverGPIOLed1_o] = {
-			.GPIO_Pin_Port = 0,
-			.GPIO_Pin_Number = 2,
-			.GPIO_Mode = GPIO_DriverGPIO_MODE_OUTPUT,
-			.GPIO_PullUpDown_Selector = GPIO_DriverGPIO_FLOATING},
-		[GPIO_DriverGPIOLed2_o] = {.GPIO_Pin_Port = 0, .GPIO_Pin_Number = 5, .GPIO_Mode = GPIO_DriverGPIO_MODE_OUTPUT, .GPIO_PullUpDown_Selector = GPIO_DriverGPIO_FLOATING},
-		[GPIO_DriverGPIOButton1_i] = {.GPIO_Pin_Port = 0, .GPIO_Pin_Number = 34, .GPIO_Mode = GPIO_DriverGPIO_MODE_INPUT, .GPIO_PullUpDown_Selector = GPIO_DriverGPIO_PULLUP_ONLY},
-		[GPIO_DriverGPIOButton2_i] = {.GPIO_Pin_Port = 0, .GPIO_Pin_Number = 35, .GPIO_Mode = GPIO_DriverGPIO_MODE_INPUT, .GPIO_PullUpDown_Selector = GPIO_DriverGPIO_PULLUP_ONLY}};
-
 GPIO_DriverRetVal_e gpioDriver_Init(GPIO_DriverGPIOConfig_s *pGPIOPinsArray_i, uint8_t GPIOPinsArrayLen_i);
 GPIO_DriverRetVal_e gpioDriver_DeInit(GPIO_DriverGPIOConfig_s *pGPIOPinsArray_i, uint8_t GPIOPinsArrayLen_i);
 
